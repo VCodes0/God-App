a340bd3a7a94c848cde0c6fee475aa5234f30b44
VCodes0 <orewavishnuprajapati@gmail.com>
2025-07-01 14:39:09 +0530

    Aarti / Wallpaper / Downloads Page Created

---
+++ lib/view/API Screens/aarti_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Aarti extends StatelessWidget {
+  const Aarti({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("Aarti")));
+  }
+}
